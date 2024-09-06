ae06f0d453e8f4c41396adc57cb43e6f42e61332
M10tech <M10@m10tech.nl>
2024-09-06 22:39:43 +0200

    0.0.3 make switch accessory without gpio
    - the objective is to be as bare as possible, no gpio
    - remember that in 0.0.2 we added PRIV_REQUIRES esp32-homekit
    - still, after pairing and disconnect, it does not work more
    - mdns might not be doing it's magic
    - debug time, let's commit this step

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## history
 
+
+### 0.0.3 make switch accessory without gpio
+- the objective is to be as bare as possible, no gpio
+- remember that in 0.0.2 we added PRIV_REQUIRES esp32-homekit
+- still, after pairing and disconnect, it does not work more
+- mdns might not be doing it's magic
+- debug time, let's commit this step
+
 ### 0.0.2 setup components
 - esp32-homekit as a git component
 - wolfssl and mdns as managed components from inside esp32-homekit
--- main/main.c
+++ main/main.c
@@ -8,11 +8,75 @@
 #include "nvs_flash.h"
 #include "nvs.h"
 #include "esp_wifi.h"
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
 
 // You must set VERSION=x.y.z of the code to match github version tag x.y.z via e.g. version.txt file
 
+static homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(ON, false);
+
+static void accessory_identify(homekit_value_t _value) {
+        printf("Accessory identify\n");
+}
+
+#define DEVICE_NAME "HomeKit Switch"
+#define DEVICE_MANUFACTURER "M10tech"
+#define DEVICE_SERIAL "1234567890"
+#define DEVICE_MODEL "bare_demo"
+#define FW_VERSION "0.0.3"
+
+static homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
+static homekit_characteristic_t manufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER, DEVICE_MANUFACTURER);
+static homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, DEVICE_SERIAL);
+static homekit_characteristic_t model = HOMEKIT_CHARACTERISTIC_(MODEL, DEVICE_MODEL);
+static homekit_characteristic_t revision = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, FW_VERSION);
+
+static homekit_accessory_t *accessories[] = {
+        HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_switches, .services = (homekit_service_t*[]) {
+                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
+                        &name,
+                        &manufacturer,
+                        &serial,
+                        &model,
+                        &revision,
+                        HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
+                        NULL
+                }),
+                HOMEKIT_SERVICE(SWITCH, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
+                        HOMEKIT_CHARACTERISTIC(NAME, "HomeKit Switch"),
+                        &switch_on,
+                        NULL
+                }),
+                NULL
+        }),
+        NULL
+};
+
+static homekit_server_config_t config = {
+        .accessories = accessories,
+        .password = "778-33-887",
+        .setupId = "2R9K",
+};
+
+static void on_wifi_ready() {
+        printf("Starting HomeKit server...\n");
+        homekit_server_init(&config);
+}
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+        if (event_base == WIFI_EVENT && (event_id == WIFI_EVENT_STA_START || event_id == WIFI_EVENT_STA_DISCONNECTED)) {
+                printf("Connecting to WiFi...\n");
+                esp_wifi_connect();
+        } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+                printf("WiFi connected, IP obtained\n");
+                on_wifi_ready();
+        }
+}
+
 void main_task(void *arg) {
     printf("the beginning\n");
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
     
     while (true) {
         printf("hi there\n");
